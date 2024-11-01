371ccf5f7f134741f51c16f058bc7675a63738ee
kentkentucky <kentloo2413@gmail.com>
2024-11-02 01:05:16 +0800

    guess the word

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ function App() {
   const [currWord, setCurrentWord] = useState(getRandomWord());
   // guessedLetters stores all letters a user has guessed so far
   const [guessedLetters, setGuessedLetters] = useState([]);
+  // store value of input
+  const [value, setValue] = useState("");
+  // number of guesses
+  const [guesses, setGuesses] = useState(10);
+  // game state
+  const [isAlive, setIsAlive] = useState(true);
+  // win state
+  const [hasWon, setHasWon] = useState(false);
 
   // Add additional states below as required.
 
@@ -21,10 +29,33 @@ function App() {
         wordDisplay.push("_");
       }
     }
+    checkWord(wordDisplay);
     return wordDisplay.toString();
   };
 
   // create additional function to power the
+  
+  // check for amount of guesses
+  const checkGuesses = () => {
+    if(guesses <= 0 || guesses > 10)
+    {
+      setIsAlive(false);
+    };
+  };
+
+  // check for winning condition
+  const checkWord = (letters) => {
+    let word = "";
+    for (let letter of letters)
+    {
+      word += letter;
+    }
+    if(word == currWord)
+    {
+      setHasWon(true);
+      setIsAlive(false);
+    }
+  };
 
   return (
     <>
@@ -34,12 +65,53 @@ function App() {
       <div className="card">
         <h1>Guess The Word 🚀</h1>
         <h3>Word Display</h3>
-        {generateWordDisplay()}
+        {isAlive && generateWordDisplay()}
         <h3>Guessed Letters</h3>
         {guessedLetters.length > 0 ? guessedLetters.toString() : "-"}
         <br />
         <h3>Input</h3>
         {/* Insert form element here */}
+        <form className="input">
+          <label>
+            Input Letter : 
+            <input 
+              type="text" 
+              maxLength={1} 
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            >
+            </input>
+          </label>
+          <button 
+            onClick={(e) => {
+              // Guess
+              if(isAlive)
+              {
+                setGuessedLetters([...guessedLetters, value]);
+                setValue("");
+                e.preventDefault();
+                setGuesses(guesses - 1);
+              }
+              // Reset
+              else
+              {
+                setCurrentWord(getRandomWord());
+                setGuesses(10);
+                setGuessedLetters([]);
+                setIsAlive(true);
+                setHasWon(false);
+              }
+            }}
+          >
+            {isAlive ? "Guess!" : "Restart"}
+          </button>
+        </form>
+        {isAlive && checkGuesses()}
+        {isAlive && <p>Guesses: {guesses}</p>}
+        {/* Display the win or loss message */}
+        {!isAlive && !hasWon && <p>You have lost!</p>}
+        {hasWon && <p>You have won!</p>}
+        {!isAlive && <p>Word: {currWord}</p>}
       </div>
     </>
   );
